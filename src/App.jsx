703815b0f0ff53901ac96b02c0a504c3d4a9cebf
Oleby0406 <oleg.bychenkov@gmail.com>
2023-04-03 21:11:31 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Message from './Message';
+import Nav from './Nav';
+import { Fragment } from "react";
+import './App.css'
+
+function App() {
+  return (
+  <Fragment>
+    <Nav></Nav>
+    <Message></Message>
+  </Fragment>
+  )
+}
+
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+    <nav className="navBar">   
+        <a className="navA">About</a>
+        <a className="navA">Experience</a>
+        <a className="navA">Projects</a>
+    </nav>
+    );
+}
+
+export default Nav
